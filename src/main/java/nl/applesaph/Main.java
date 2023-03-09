067fdb9dd774c61edff43ad066c2938e01ebb25c
Gerwin Trip <gerwintrip@gmail.com>
2023-03-09 13:23:56 +0100

    main

--- src/main/java/nl/applesaph/Main.java
+++ src/main/java/nl/applesaph/Main.java
@@ -1,5 +1,44 @@
-package nl.applesaph;public class Main {
+package nl.applesaph;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static boolean exit = false;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        boolean valid;
+        int port = 0;
+        do {
+            System.out.println("Please enter a port number (or leave empty for 55555):");
+            valid = true;
+            try {
+                String input = scanner.nextLine();
+                if (input.equals("")) {
+                    //default port if input is empty
+                    port = 55555;
+                    break;
+                }
+                port = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                //if input isn't a number
+                System.out.println("ERROR: Not a valid port");
+                valid = false;
+            }
+            if (!(0 <= port && port <= 65536)) {
+                //if input is out of range
+                System.out.println("ERROR: Port number is invalid");
+                valid = false;
+            }
+        } while (!valid);
+        Server s = new Server(port);
+        s.start();
+        while (!exit) {
+            if (scanner.nextLine().equals("quit")) {
+                exit = true;
+            }
+        }
+        s.stop();
     }
 }
